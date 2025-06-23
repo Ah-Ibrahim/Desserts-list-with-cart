7f23739e1774bc5ed9716333941aa1dc066cfd42
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-06-23 13:12:59 +0300

    Added Initial DesertCard UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import './App.css';
 import DesertsContainer from './components/DesertsContainer';
 import DesertsCard from './components/DesertsCard';
 
+const itemData = {
+	id: 1,
+	imgSrc: '/images/image-waffle-desktop.jpg',
+	category: 'Waffle',
+	name: 'Waffle with Berries',
+	price: 6.5,
+	quantity: 0,
+};
+
 function App() {
 	return (
 		<main className="app">
-			<DesertsCard />
+			<DesertsCard itemData={itemData} />
 		</main>
 	);
 }
--- src/components/DesertsCard.jsx
+++ src/components/DesertsCard.jsx
@@ -1,36 +1,62 @@
 import './DesertsCard.css';
 import addToCartImg from '../assets/images/icon-add-to-cart.svg';
+import iconIncrement from '../assets/images/icon-increment-quantity.svg';
+import iconDecrement from '../assets/images/icon-decrement-quantity.svg';
+import { useEffect } from 'react';
 
-function DesertsCardButton({ count = 1, onClick, children }) {
-	if (count) {
+function DesertsCardButton({ count = 0, onClick, children }) {
+	if (!count) {
 		return (
-			<button className="btn" onClick={onClick}>
+			<button className="btn btn--main" onClick={onClick}>
 				<img className="img" src={addToCartImg} /> {children}
 			</button>
 		);
 	} else {
-		return <div className="btn"></div>;
+		return (
+			<div className="btns-container">
+				<button className="btn btn--icon" aria-label="Decrement Button">
+					<img src={iconDecrement} alt="Decrement Icon" />
+				</button>
+				<div className="btns-container__count">{count}</div>
+				<button className="btn btn--icon" aria-label="Increment Button">
+					<img src={iconIncrement} alt="Increment Icon" />
+				</button>
+			</div>
+		);
 	}
 }
 
-function DesertsCard({ count = 0, itemData }) {
-	const itemCategory = itemData?.image?.thumbnail ?? 'Unknown';
+// NOTE: Item Data
+// const itemData = {
+// 	id: 0,
+//  imgSrc : '',
+// 	category: '',
+// 	name: '',
+// 	price: 0,
+// 	quantity: 0,
+// };
+
+function DesertsCard({ itemData }) {
+	console.log(itemData);
+	const itemId = itemData?.id ?? 0;
+	const itemImgSrc = itemData?.imgSrc ?? {};
+	const itemCategory = itemData?.category ?? 'Unknown';
 	const itemName = itemData?.name ?? 'Unknown';
 	const itemPrice = itemData?.price ?? 0;
-	const itemImgSrc = itemData?.image?.thumbnail;
+	const itemQuantity = itemData?.quantity ?? 0;
 
 	const handleClick = function () {};
 
 	return (
 		<div className="item">
-			<div className="item__img">
+			<div className={`item__figure ${itemQuantity ? 'item__figure--selected' : ''}`}>
 				<img src={itemImgSrc} alt="Item Image" />
 				<DesertsCardButton onClick={handleClick}>Add to Cart</DesertsCardButton>
 			</div>
 			<div className="item__desc">
 				<div className="category">{itemCategory}</div>
 				<div className="name">{itemName}</div>
-				<div className="price">{itemPrice}</div>
+				<div className="price">${itemPrice.toFixed(2)}</div>
 			</div>
 		</div>
 	);
