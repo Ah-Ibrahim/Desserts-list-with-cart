1770183595f550132f7dd1a6dadd09f5344e34d4
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-06-23 13:12:59 +0300

    Completed UI of DesertCard Component

--- src/components/DesertsList.jsx
+++ src/components/DesertsList.jsx
@@ -1,17 +1,27 @@
 import './DesertsList.css';
 import DesertsCard from './DesertsCard';
 
+const itemData = {
+	id: 1,
+	imgSrc: '/',
+	category: 'Waffle',
+	name: 'Waffle with Berries',
+	price: 6.5,
+	quantity: 0,
+};
+
 function DesertsList({ list }) {
-	const items = list.map((item, index) => {
-		<li key={index}>
-			<DesertsCard />
-		</li>;
-	});
+	// const items = list.map((item, index) => {
+	// 	<li key={index}>
+	// 		<DesertsCard />
+	// 	</li>;
+	// });
 
 	return (
 		<div>
 			<h1>Deserts</h1>
-			<ul>{items}</ul>
+			{/* <ul>{items}</ul> */}
+			<DesertsCard itemData={itemData} />
 		</div>
 	);
 }
