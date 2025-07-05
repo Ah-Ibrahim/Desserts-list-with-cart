ba391a1f13fd1d7ca2064811fdb948ef394eda1a
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-07-05 15:51:16 +0300

    Fixed Base address

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/Deserts-list-with-cart/',
+	base: '/Desserts-list-with-cart/',
 });
